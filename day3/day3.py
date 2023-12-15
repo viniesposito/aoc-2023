26ba9e612e87b2615d227efc1ca9dbf99509b3f0
viniesposito <viniesposito@gmail.com>
2023-12-15 13:33:28 -0500

    day 3 done

--- day3/day3.py
+++ day3/day3.py
@@ -1,4 +1,5 @@
 import re
+from math import prod
 
 test = """
 467..114..
@@ -48,4 +49,50 @@ with open("day3\input", "r") as input:
 
 print(f"Part 1 output = {part1(input)} \n")
 
+def part2(input):
 
+    input = [val for val in input.splitlines() if val != ""]
+    
+    n = len(input)
+
+    data = [[[[], False] for y in x] for x in input]
+
+    for r, line in enumerate(input):
+
+        for match in re.finditer(r"\d+", line):
+            s = match.start()
+            e = match.end()
+            m = len(line)
+
+            adjacent = [val[max(0, s-1):min(e+1,m)] for val in input[max(0,r-1):(1+min(r+1,n))]]
+
+            if [val for val in "".join(adjacent) if val != "." and not val.isdigit()]:
+                num = int(line[s:e])
+                
+
+                for i in range(max(0,r-1),min(r+2,n)):
+                    for j in range(max(0,s-1),(min(e+1,m))):
+                        
+                        data[i][j][0].append(num)
+                        
+                        if input[i][j] == "*":
+                            data[i][j][1] = True
+                        
+    gears = [[y for y in x if len(y[0]) == 2 and y[1]] for x in data]
+
+    score = 0
+
+    gear_ratios = []
+
+    for gear in gears:
+        if gear:
+            for entry in gear:
+                
+                score += entry[0][0]*entry[0][1]
+                gear_ratios.append(entry[0][0]*entry[0][1])
+
+    return score
+
+print(f"Part 2 test output = {part2(test)} \n")
+
+print(f"Part 2 output = {part2(input)} \n")
