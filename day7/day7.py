aa52ee272ddececf7fb8f03f4875f205a52d6497
viniesposito <viniesposito@gmail.com>
2023-12-26 14:32:44 -0300

    day 7 part 1 done

--- day7/day7.py
+++ day7/day7.py
@@ -1,8 +1,10 @@
+from collections import Counter
+from functools import cmp_to_key
+
 test = """
 32T3K 765
 T55J5 684
 KK677 28
-KKK67 98
 KTJJT 220
 QQQJA 483
 """
@@ -28,70 +30,56 @@ def part1(input):
 
     input = [x.split(" ") for x in input.splitlines() if x != ""]
 
-    n_hands = len(input)
+    def get_hand_type(hand):
 
-    score_list = []
-    hand_pts_list = []
-
-    for hand_list in input:
-        hand = hand_list[0]
-
-        card_count_list = []
-
-        for item in card_map.keys():
-            card_count = hand.count(item)
-
-            if card_count > 0:
-                card_count_list.append(card_count)
+        card_count_list = list(Counter(hand).values())
 
         if 5 in card_count_list:
-            score = 6
+            hand_type = 6
         elif 4 in card_count_list:
-            score = 5
+            hand_type = 5
         elif 3 in card_count_list and 2 in card_count_list:
-            score = 4
+            hand_type = 4
         elif 3 in card_count_list:
-            score = 3
+            hand_type = 3
         elif card_count_list.count(2) == 2:
-            score = 2
+            hand_type = 2
         elif 2 in card_count_list:
-            score = 1
+            hand_type = 1
         else:
-            score = 0
+            hand_type = 0
 
-        score_list.append(score)
-        hand_pts_list.append([card_map[card] for card in hand])
+        return hand_type
 
-    score_list = [1+sorted(score_list).index(x) for x in score_list]
-    input = [input[i]+[score_list[i]] for i in range(n_hands)]
-    input = [input[i]+[hand_pts_list[i]] for i in range(n_hands)]
-    # each entry in input = [hand string, bid string, score int, hand pts list of int]
-    sorted_input = [x for _, x in sorted(zip(score_list, input))]
-    print(sorted(score_list))
-    for i in range(1, n_hands):
-        print(f"Looking at hand {i} vs {i-1}: {sorted_input[i][0]} vs {sorted_input[i-1][0]}")
-        score = sorted_input[i][2]
-        score_prev = sorted_input[i-1][2]
-        if score == score_prev:
-            print(f"Tie found! Two cards with score = {score}")
-            for j in range(5):
-                if sorted_input[i][3][j] > sorted_input[i-1][3][j]:
-                    sorted_input[i][2] += 1
-                    print(f"Hand {i} won! Its score was bumped to {sorted_input[i][2]}\n")
-                    break
-                elif sorted_input[i][3][j] < sorted_input[i-1][3][j]:
-                    sorted_input[i-1][2] += 1
-                    print(f"Hand {i-1} won! Its score was bumped to {sorted_input[i-1][2]}\n")
-                    break
-            print(i)        
-            if sorted_input[(i+1):]:
-                print("fuck",i)
-                print([sorted_input[k][2] for k in range(i+1,n_hands)])
-                # sorted_input[(i+1):] = [val[2]+1 for val in sorted_input[(i+1):]]
-                
+    def compare_hands(hand1, hand2):
+        type1 = get_hand_type(hand1[0])
+        type2 = get_hand_type(hand2[0])
 
-    print(sorted_input)
+        if type1 > type2:
+            return 1
+        if type1 < type2:
+            return -1
+        else:
+            for i in range(5):
+                if hand1[0][i] != hand2[0][i]:
+                    if card_map[hand1[0][i]] > card_map[hand2[0][i]]:
+                        return 1
+                    else:
+                        return -1
 
+    input = sorted(input, key=cmp_to_key(compare_hands))
+    
+    score = 0
 
+    for i, hand in enumerate(input):
+        bid = int(hand[1])
+        score += (i+1) * bid
 
-part1(test)
+    return score
+
+print(f"Part 1 test output = {part1(test)} \n")
+
+with open("day7\input", "r") as input:
+    input = "".join([val for val in input])
+
+print(f"Part 1 output = {part1(input)} \n")
