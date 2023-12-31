a1bf72e4918e321fa26892e53664972d49d4bd04
viniesposito <viniesposito@gmail.com>
2023-12-30 22:39:05 -0300

    meh

--- day10/day10.py
+++ day10/day10.py
@@ -65,3 +65,63 @@ with open("day10\input", "r") as input:
     input = "".join([val for val in input])
 
 print(f"Part 1 output = {part1(input)} \n")
+
+def part2(input):
+
+    input = [x for x in input.splitlines() if x != ""]
+    
+    map = {}
+
+    for i, row in enumerate(input):
+        for j, node in enumerate(row):
+            
+            adjacent = []
+
+            if node in "F|7":
+                adjacent.append((i+1,j))
+            if node in "|LJ":
+                adjacent.append((i-1,j))
+            if node in "-J7":
+                adjacent.append((i,j-1))
+            if node in "F-L":
+                adjacent.append((i,j+1))
+            if node == "S":
+                start = (i,j)
+                
+            map[(i,j)] = adjacent
+
+    start_adjacent = [k for k, v in map.items() if start in v]
+    
+    a = start_adjacent[0]
+    b = start_adjacent[1]
+    
+    loop = [start]
+
+    while True:
+        loop.append(a)
+        loop.append(b)
+        
+        if a == b:
+            break
+
+        a = [x for x in map[a] if x not in loop][0]
+        b = [x for x in map[b] if x not in loop][0]
+
+    for i, row in enumerate(input):
+        for j, node in enumerate(row):
+            
+            # if (i,j) not in loop and node == "." 
+
+test3 = """
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+"""
+
+part2(test3)
