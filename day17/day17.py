9867aefe551379c147d65fd1b2fe8af8c1bf3704
viniesposito <viniesposito@gmail.com>
2024-01-14 17:34:02 -0500

    getting rekt by capping same direction move at only 3 consecutive times

--- day17/day17.py
+++ day17/day17.py
@@ -32,45 +32,76 @@ def part1(input):
         
         return True
 
-    def get_neighbors(input, pos):
+    def get_neighbors(input, pos, direction, k):
         x, y = pos
 
-        for dx in [-1, 0, 1]:
-            for dy in [-1, 0, 1]:
-                if dx == 0 and dy == 0:
-                    continue
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            new_pos = x + dx, y + dy
 
-                new_pos = x + dx, y + dy
+            if (dx,dy) == direction:
+                k += 1
+            else:
+                k = 1
 
-                if is_valid(input, new_pos):
-                    yield new_pos
+            if is_valid(input, new_pos) and k <= 3:
+                yield new_pos
 
-    def get_shorter_paths(tentative, positions, through):
+    def score_path(input, path):
+        score = 0
+
+        if path[0] == (0,0):
+            s = 1
+        else:
+            s = 0
+
+        for x,y in path[s:]:
+            score += int(input[x][y])
+
+        return score
+
+    def get_shorter_paths(input, tentative, positions, through):
         path = tentative[through] + [through]
 
         for pos in positions:
-            if pos in tentative and len(tentative[pos]) <= len(path):
+            if pos in tentative and score_path(input, tentative[pos]) > score_path(input, path):
                 continue
             yield pos, path
 
     tentative = {origin: []}
-    queue = [(0, origin)]
+    queue = [(0, origin, (0, 0), 1)]
     certain = set()
 
     while destination not in certain and queue:
-        _, current = heapq.heappop(queue)
+        _, current, direction, k = heapq.heappop(queue)
         if current in certain:
             continue
         certain.add(current)
-        neighbors = set(get_neighbors(input, current)) - certain
-        shorter = get_shorter_paths(tentative, neighbors, current)
+        neighbors = set(get_neighbors(input, current, direction, k)) - certain
+        shorter = get_shorter_paths(input, tentative, neighbors, current)
         for neighbor, path in shorter:
             tentative[neighbor] = path
-            heapq.heappush(queue, (len(path), neighbor))
+            new_direction = (neighbor[0] - current[0], neighbor[1] - current[1])
+            if new_direction == direction:
+                k += 1
+                if k > 3:
+                    continue
+            else:
+                k = 1
+            heapq.heappush(queue, (score_path(input, path + [neighbor]), neighbor, new_direction, k))
     if destination in tentative:
         best_path = tentative[destination] + [destination]
 
-    return len(best_path)
+    print(score_path(input, best_path))
+
+    return best_path
+
+def show_path(path, input):
+    input = input.strip().splitlines()
+    for x, y in path:
+        input[x] = input[x][:y] + "@" + input[x][y + 1 :]
+    return "\n".join(input) + "\n"
+
+print(show_path(part1(test),test))
 
 print(f"Part 1 test output = {part1(test)} \n")
 
