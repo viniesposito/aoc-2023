d6974f6abe42d4d4372eaa9573ae30bede4af657
viniesposito <viniesposito@gmail.com>
2024-01-14 15:07:55 -0500

    day 17 sketch

--- day17/day17.py
+++ day17/day17.py
@@ -1,3 +1,5 @@
+import heapq
+
 test = """
 2413432311323
 3215453535623
@@ -15,7 +17,60 @@ test = """
 """
 
 def part1(input):
-    pass
+    
+    input = input.strip().splitlines()
+    origin = 0, 0
+    destination = len(input) - 1, len(input[0]) - 1
+
+    def is_valid(input, pos):
+        x, y = pos
+        m = len(input)
+        n = len(input[0])
+
+        if not (0 <= x < m and 0 <= y < n):
+            return False
+        
+        return True
+
+    def get_neighbors(input, pos):
+        x, y = pos
+
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                if dx == 0 and dy == 0:
+                    continue
+
+                new_pos = x + dx, y + dy
+
+                if is_valid(input, new_pos):
+                    yield new_pos
+
+    def get_shorter_paths(tentative, positions, through):
+        path = tentative[through] + [through]
+
+        for pos in positions:
+            if pos in tentative and len(tentative[pos]) <= len(path):
+                continue
+            yield pos, path
+
+    tentative = {origin: []}
+    queue = [(0, origin)]
+    certain = set()
+
+    while destination not in certain and queue:
+        _, current = heapq.heappop(queue)
+        if current in certain:
+            continue
+        certain.add(current)
+        neighbors = set(get_neighbors(input, current)) - certain
+        shorter = get_shorter_paths(tentative, neighbors, current)
+        for neighbor, path in shorter:
+            tentative[neighbor] = path
+            heapq.heappush(queue, (len(path), neighbor))
+    if destination in tentative:
+        best_path = tentative[destination] + [destination]
+
+    return len(best_path)
 
 print(f"Part 1 test output = {part1(test)} \n")
 
