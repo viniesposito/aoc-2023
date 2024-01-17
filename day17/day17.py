f330c4937ba6509bf3497608fc635362a3b7d881
viniesposito <viniesposito@gmail.com>
2024-01-16 21:55:07 -0500

    day 17 done

--- day17/day17.py
+++ day17/day17.py
@@ -60,3 +60,45 @@ with open("day17\input", "r") as input:
     input = "".join([val for val in input])
 
 print(f"Part 1 output = {part1(input)} \n")
+
+def part2(input):
+    
+    input = input.strip().splitlines()
+    input = [[int(c) for c in r] for r in input]
+
+    origin = 0,0
+    end = len(input) - 1, len(input[0]) -1
+    m, n = end
+    history = set()
+    directions = [(-1,0),(1,0),(0,-1),(0,1)]
+    queue = [(0, origin, (0,1), 0),(0, origin, (1,0), 0)]
+
+    while queue:
+        load, pos, direction, k = heapq.heappop(queue)
+        x, y = pos
+        dx, dy = direction
+        if pos == end:
+            break
+        if (pos, direction, k) in history:
+            continue
+        history.add((pos, direction, k))
+        for new_direction in directions:
+            new_dx, new_dy = new_direction
+            if new_dx == -dx and new_dy == -dy:
+                continue
+            new_x, new_y = x + new_dx, y + new_dy
+            new_pos = new_x, new_y
+            if new_x < 0 or new_y < 0 or new_x > m or new_y > n:
+                continue
+            going_straight = (new_direction == direction)
+            if (going_straight and k == 10) or (k < 4 and not going_straight):
+                continue
+            new_k = k + 1 if going_straight else 1
+            new_load = load + input[new_x][new_y]
+            heapq.heappush(queue, (new_load, new_pos, new_direction, new_k))
+
+    return load
+
+print(f"Part 2 test output = {part2(test)} \n")
+
+print(f"Part 2 output = {part2(input)} \n")
