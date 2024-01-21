463c2ec99e62a172acf576984ad721355193858f
viniesposito <viniesposito@gmail.com>
2024-01-21 12:05:08 -0500

    day 19 done

--- day19/day19.py
+++ day19/day19.py
@@ -1,4 +1,5 @@
 import re
+from math import prod
 
 test = """
 px{a<2006:qkq,m>2090:A,rfg}
@@ -79,42 +80,49 @@ def part2(input):
     def parse_workflows(workflows):
         d = {}
         for wf in workflows:
+            rules_list = []
             wf_name, rules = re.findall(r"(\w+){(.+)}", wf)[0]
-            rules = [r.split(":") if ":" in r else r for r in rules.split(",")]
-            d[wf_name] = rules
+            for char, operator, num, dest in re.findall(r"(\w+)(<|>)(\d+):(\w+)", rules):
+                rules_list.append((char, operator, int(num), dest))
+            d[wf_name] = rules_list + [rules.split(",")[-1]]  
         return d
-    
-    def rate_ratings(rating):
-        score = 0
-        x, m, a, s = [int(num) for num in re.findall(r"{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}", rating)[0]]
-        wf = "in"
-        step = "Z"
-        while True:
-            if wf == "A":
-                score += (x+m+a+s)
-                break
-            elif wf == "R":
-                break
-            
-            for step in workflows[wf]:
-                if isinstance(step, list): 
-                    if eval(step[0]):
-                        wf = step[1]
-                        break
-                    else:
-                        continue
-                else:
-                    wf = step
-        
-        return score
-    
+
     workflows, _ = parse_input(input)
     workflows = parse_workflows(workflows)
     
-    x = 0
-    for rating in ratings:
-        x += rate_ratings(rating)
-    return x
+    queue = [("in", (1, 4_000), (1, 4_000), (1, 4_000), (1, 4_000))]
+    score = 0
+
+    while queue:
+        wf_name, *xmas_ranges = queue.pop()
+        if wf_name == "A":
+            score += prod(1 + x[1] - x[0] for x in xmas_ranges)
+            continue
+        elif wf_name == "R":
+            continue
+
+        for char, operator, num, dest in workflows[wf_name][:-1]:
+            inf, sup = xmas_ranges["xmas".index(char)]
+            if (num > sup and operator == "<") or (num < inf and operator == ">"):
+                break
+            if (num >= sup and operator == ">") or (num <= inf and operator == "<"):
+                continue
+            if operator == ">":
+                passes = (num + 1, sup)
+                fails = (inf, num)
+            else:
+                fails = (num, sup)
+                passes = (inf, num - 1)
+            xmas_ranges["xmas".index(char)] = fails
+            new_xmas_ranges = xmas_ranges[:]
+            new_xmas_ranges["xmas".index(char)] = passes
+            queue.append((dest, *new_xmas_ranges))
+        
+        else:
+            queue.append((workflows[wf_name][-1], *xmas_ranges))
+
+    return score
+    
 
 print(f"Part 2 test output = {part2(test)} \n")
 
